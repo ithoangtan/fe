91f04ef06ff561179a35b8b734a11ee3509dcca6
ithoangtan <ithoangtan@gmail.com>
2021-08-12 16:19:31 +0700

    eject react testing lib and init type sciprt project

--- react-testing-library/src/CookieConsent.js
+++ react-testing-library/src/CookieConsent.js
@@ -1,27 +1,26 @@
-import {useCookieConsent} from 'use-cookie-consent';
+import { useCookieConsent } from "use-cookie-consent";
 
 export const CookieConsent = () => {
-  const {consent, acceptAllCookies, declineAllCookies, acceptCookies} =
+  const { consent, acceptAllCookies, declineAllCookies, acceptCookies } =
     useCookieConsent();
 
   return (
     <div className="">
       <h3>
-        {`Third-party cookies ${consent.thirdParty ? 'approved' : 'rejected'}`}
+        {`Third-party cookies ${consent.thirdParty ? "approved" : "rejected"}`}
       </h3>
       <h3>
-        {`First-party cookies ${consent.firstParty ? 'approved' : 'rejected'}`}
+        {`First-party cookies ${consent.firstParty ? "approved" : "rejected"}`}
       </h3>
 
       <button onClick={acceptAllCookies}>Accept all</button>
-      <button onClick={() => acceptCookies({thirdParty: true})}>
+      <button onClick={() => acceptCookies({ thirdParty: true })}>
         Accept third-party
       </button>
-      <button onClick={() => acceptCookies({firstParty: true})}>
+      <button onClick={() => acceptCookies({ firstParty: true })}>
         Accept first-party
       </button>
       <button onClick={declineAllCookies}>Reject all</button>
     </div>
   );
 };
-
